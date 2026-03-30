4b96b41c8df5a4ac1383705435b1d6b42c90756c
Guilherme <borutoplays038@gmail.com>
2026-03-30 09:15:33 -0300

    feat: Declarações e variáveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,44 @@
-alert("Hello World !!!")
+//  Declarações e variáveis
+
+
+let nome ="Fiap";
+
+console.log(nome);
+
+nome = "Teste";
+console.log(nome)
+
+if(true){
+    var apelido = "Fiapinho"
+}
+
+console.log(apelido);
+
+const taxaJuros = 0.05;
+console.log(taxaJuros);
+//taxaJuros = 1.5;
+//console.log(taxaJuros);
+
+var exemplo="ola-dev-var";
+console.log(exemplo);
+
+let exemplo1="ola-dev-let";
+console.log(exemplo1);
+
+const exemplo2="ola-dev-let";
+console.log(exemplo2);
+
+let exemplo3=10;
+console.log(exemplo3);
+
+let exemplo5 = {nome1:"teste"};
+console.log(exemplo5);
+
+let exemplo6 = ["Java","C#","PHP"];
+console.log(exemplo6);
+
+let exemplo7;
+console.log(exemplo7);
+
+let exemplo8 = null;
+console.log(exemplo8)
